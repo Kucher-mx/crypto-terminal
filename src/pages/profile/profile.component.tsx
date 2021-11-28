aef3dbf78900a223dd68e9eb8561b286b71e2f37
Kucher Maxym <max.kucher98@gmail.com>
2021-11-28 21:16:43 +0200

    added profile view

--- src/pages/profile/profile.component.tsx
+++ src/pages/profile/profile.component.tsx
@@ -1,28 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../../components/header/header.component';
+import CheckboxCustom from '../../components/inputs/checkbox/checkbox.component';
 import Input from '../../components/inputs/input/input.component';
+import Switch from '../../components/inputs/switch/switch.component';
 import { updateUserDoc } from '../../firebase/firebase';
 import './profile.styles.css';
+import { ReactComponent as UserIcon } from '../../components/inputs/input/assets/akar-icons_person.svg';
 
 const Profile = () => {
   const [state, setstate] = useState({
     apiKey: '',
     secretApiKey: '',
+    risk: false,
+    stopLossCheckBox: false,
+    stopLoss: '',
+    takeProfitCheckBox: false,
+    takeProfit: '',
   });
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-
+    const { value, name, type, checked } = e.target;
     setstate({
       ...state,
-      [name]: value,
+      [name]: type === 'checkbox' ? checked : value,
     });
   };
+
   useEffect(() => {
     const json = sessionStorage.getItem('userData');
     if (json) {
       const parsed = JSON.parse(json);
-      setstate({ apiKey: parsed.apiKey, secretApiKey: parsed.secretApiKey });
+      //setstate({ apiKey: parsed.apiKey, secretApiKey: parsed.secretApiKey });
     }
   }, []);
 
@@ -39,9 +47,11 @@ const Profile = () => {
       <Header />
       <div className="profile">
         <div className="profile_content">
-          <div className="profile_email"></div>
-          <div className="input-wrapper">
-            <form className="profile_form" onSubmit={onSubmitHandler}>
+          <div className="profile_email">
+            <UserIcon /> max.kucher98@gmail.com
+          </div>
+          <form className="profile_form" onSubmit={onSubmitHandler}>
+            <div className="api_block">
               <Input
                 placeholder="enter your api key"
                 type="text"
@@ -56,11 +66,63 @@ const Profile = () => {
                 onChange={e => onChangeHandler(e)}
                 name="secretApiKey"
               />
-              <button type="submit" className="form-submit">
-                Save
-              </button>
-            </form>
-          </div>
+            </div>
+
+            <div className="risks_block">
+              <div className="title">Risks</div>
+              <Switch
+                name={'risk'}
+                id={'risk'}
+                onChange={onChangeHandler}
+                value={state.risk}
+                options={['Auto', 'Manual']}
+              />
+              <div className="risks_inputs">
+                <div className="profile-input-block">
+                  <div className="label">
+                    <CheckboxCustom
+                      name={'stopLossCheckBox'}
+                      id={'stopLossCheckBox'}
+                      onChange={onChangeHandler}
+                      value={state.stopLossCheckBox}
+                    />
+                    Stop-Loss
+                  </div>
+
+                  <Input
+                    placeholder="stopLoss"
+                    type="text"
+                    value={state.stopLoss}
+                    onChange={onChangeHandler}
+                    name="stopLoss"
+                  />
+                </div>
+                <div className="profile-input-block">
+                  <div className="label">
+                    <CheckboxCustom
+                      name={'takeProfitCheckBox'}
+                      id={'takeProfitCheckBox'}
+                      onChange={onChangeHandler}
+                      value={state.takeProfitCheckBox}
+                    />
+                    Take-Profit
+                  </div>
+
+                  <Input
+                    placeholder="enter your api secret key"
+                    type="text"
+                    value={state.takeProfit}
+                    onChange={onChangeHandler}
+                    name="takeProfit"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <button type="submit" className="form-submit">
+              Save
+            </button>
+          </form>
         </div>
       </div>
     </>
